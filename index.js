74b17e2afb16745b3c65654028f65e20e4dea8f0
flowersfaerie <flowersfaerie@gmail.com>
2019-12-08 18:48:51 -0800

    Completed: TShirts By Size

--- index.js
+++ index.js
@@ -220,8 +220,11 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+	let runnersBySize = runners.filter((item, index) => {
+		return item.shirt_size === tShirtSize;
+	});
+	return runnersBySize;
 }
 
 /**
@@ -234,7 +237,7 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
+function tallyUpDonations(runners) {
   /* CODE HERE */
 }
 
